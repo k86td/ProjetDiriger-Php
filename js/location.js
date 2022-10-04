297ae4e3ac2587f9dd236fb5cbfb390fd7de0eab
Tristan <tristanlepine14@gmail.com>
2022-10-04 17:20:11 -0400

    finished DemandeOffre in location.php

--- js/location.js
+++ js/location.js
@@ -47,13 +47,16 @@ const mappingFunc = (filterName) => (d) => {
 
 const defaultCardMaker = async (j) => {
     // check to see if there's an offer detail for that location
-    var payload = await $.ajax({
-        url: BASE_URL + "/Voiture",
-        type: 'GET'
-    });
-
-    console.log(JSON.stringify(payload));
-
+    // var payload = await $.ajax({
+        //     url: BASE_URL + "/Voiture",
+        //     type: 'GET'
+        // });
+        // console.log(JSON.stringify(payload));
+    
+    // var demandeOffre = await $.ajax({
+    //     url: BASE_URL + "/DemandeOffre/"
+    // });
+        
     return `<div class="card" style="width: 18rem;"><div class="card-body"><h3 class="card-title">${j.nom}</h3><p>${j.prix}$ / jour</p></div></div>`;
 }
 
@@ -63,8 +66,36 @@ const LoadMainContent = async (queryString = "", makerFunc) => {
         throw new Error("makerFunc cannot be undefined");
     }
 
+    let user_token = $("#user_token").val();
+
+    // this is the function that creates the cards
     const parser = async (json) => {
         return await Promise.all(json.map(async j => {
+            console.debug("Debugging 'j': " + JSON.stringify(j));
+
+            let boutonAjouterDemandeOffre = "";
+            if (user_token !== undefined) {
+                try {
+                    var demandeOffre = await $.ajax({
+                        beforeSend: function (xhr) {
+                            xhr.setRequestHeader("Authorization", "Bearer " + user_token);
+                        },
+                        url: BASE_URL + "/DemandeOffre/" + j["id"],
+                        type: 'GET'
+                    });
+                    
+                    boutonAjouterDemandeOffre = `<button name="rent_button" type="buton" class="btn btn-primary" id="rent_${j["id"]}">Louer</button>`;
+                    if (demandeOffre !== undefined) {
+                        boutonAjouterDemandeOffre = `<button type="buton" class="btn btn-outline-primary" disabled>Deja louer</button>`;
+                    }
+                }
+                catch {
+                    console.error("Impossible to access that url. Re-login");
+                }
+            }
+
+            console.log(`Result for Offre[${j["id"]}] => ${demandeOffre}`)
+
             var payload = await $.ajax({
                 url: BASE_URL + "/Voiture/" + j["id"],
                 type: 'GET'
@@ -75,21 +106,42 @@ const LoadMainContent = async (queryString = "", makerFunc) => {
                 description = `<a href="offre.php?id=${j["id"]}">Description</a>`;
             }
 
-            return `<div class="card" style="width: 18rem;"><div class="card-body"><h3 class="card-title">${j["nom"]}</h3><p>${j["prix"]}$ / jour</p><br>${description}</div></div>`;
+            return `<div id="location_${j["id"]}" class="card" style="width: 18rem;"><div class="card-body"><h3 class="card-title">${j["nom"]}</h3><p>${j["prix"]}$ / jour</p><br>${description}<br>${boutonAjouterDemandeOffre}</div></div>`;
         }));
     };
 
-
-    
     const DATA_PATH = ".main-content";
     const API_PATH = "https://localhost:7103/api/Offre";
 
-    $.get(API_PATH + queryString, async function(data) {
+    $.get(API_PATH + queryString, async function (data) {
         let parsedData = await parser(data);
 
-        console.log(JSON.stringify(parsedData));
-
+        // setup the handlers for all the "Louer" buttons
         $(DATA_PATH).html(parsedData);
+
+        $("button[name='rent_button']").click(({ target }) => {
+            // call api to try to rent
+            $.ajax({
+                type: 'POST',
+                url: BASE_URL + "/Offre/Rent/" + target.id.split("_")[1], // get offre id
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Authorization", "Bearer " + user_token);
+                },
+                success: function () {
+                    target = $("#" + target.id);
+                    target.attr("disabled", true);
+                    target.removeClass("btn-primary");
+                    target.addClass("btn-outline-primary");
+                    target.html("Deja louer");
+                },
+                error: function () {
+                    target = $("#" + target.id);
+                    target.attr("disabled", false);
+                    target.removeClass("btn-primary");
+                    target.addClass("btn-outline-danger");
+                }
+            });
+        });
     })
     .fail(function () {
         setTimeout(function () {
--- location.php
+++ location.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
-
+<?php session_start() ?>
 <head>
 	<title>Location - Autorius</title>
 	<meta charset="UTF-8">
@@ -13,6 +13,8 @@
 
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+	<input id="user_token" type="hidden" value="<?php echo $_SESSION["token"]; ?>">
 </head>
 
 <body>
