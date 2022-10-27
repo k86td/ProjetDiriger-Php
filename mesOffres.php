766d206d1c5288a17f5398c773382dd772a2d64a
jouellet310 <justin_ouellet@hotmail.com>
2022-10-27 11:14:01 -0400

    Changed headerbar in mesOffres.php

--- mesOffres.php
+++ mesOffres.php
@@ -37,49 +37,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     <link rel="stylesheet" href="css/offre.css">
 </head>
 
-<body id="top" data-spy="scroll" data-target=".navbar-collapse" data-offset="50">
-
-    <!-- PRE LOADER -->
-    <section class="preloader">
-        <div class="spinner">
-            <span class="spinner-rotate"></span>
-        </div>
-    </section>
-
-
-    <!-- MENU -->
-    <section class="navbar custom-navbar navbar-fixed-top" role="navigation">
-        <div class="container">
-
-            <div class="navbar-header">
-                <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon icon-bar"></span>
-                    <span class="icon icon-bar"></span>
-                    <span class="icon icon-bar"></span>
-                </button>
-
-                <!-- lOGO TEXT HERE -->
-                <a href="#" class="navbar-brand">Autorius</a>
-            </div>
-
-            <!-- MENU LINKS -->
-            <div class="collapse navbar-collapse">
-                <ul class="nav navbar-nav navbar-nav-first">
-                    <li class="active"><a href="index.php">Accueil</a></li>
-                    <li><a href="location.php">Offres</a></li>
-                    <li><a href="about.php">À propos</a></li>
-                    <?php
-                    if (isset($_SESSION['email'])) {
-                        echo ' <li><a href="deconnection.php">Se déconnecter</a></li>';
-                    } else {
-                        echo '<li><a href="login.php">Se connecter</a></li>';
-                        echo '<li><a href="inscription.php">Inscription</a></li>';
-                    }
-                    ?>
-                </ul>
-            </div>
-        </div>
-    </section>
+<?php
+include '_headerBar.php';
+?>
 
     <section class="services" id="services">
         <div class="heading">
