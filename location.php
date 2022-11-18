2cfe97b79063d2b7b01e0e20773445408cae27cd
Tristan Lepine <tristanlepine14@gmail.com>
2022-11-17 22:04:11 -0500

    modified toast

--- location.php
+++ location.php
@@ -19,6 +19,8 @@ $active_link = "location";
 	<?php include '_header.php'; // include '_headerBar.php';  
 	?>
 
+	<div id="mainBody" class="toast-container position-fixed bottom-0 end-0 p-3"></div>
+
 	<main class="container">
 		<h2 style="width: 100%; text-align: center; margin-bottom: 25px;">🚧 en construction 🚧</h2>
 		<script>
