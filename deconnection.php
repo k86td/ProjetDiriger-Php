f4e204ea43904cc2fef284d6111af423f533f9cb
Guillaume <guillaume.richer@bell.net>
2022-11-21 22:03:58 -0500

    modify deconnection

--- deconnection.php
+++ deconnection.php
@@ -1,8 +1,6 @@
 <?php
     session_start();
-    unset($_SESSION['email']);
-    unset($_SESSION['token']);
-    unset($_SESSION['offre']);
+    session_destroy();
     header('Location: index.php');
 
 ?>
