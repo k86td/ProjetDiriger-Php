86da630d333ff4d89d546c1e0fd63b491cc96a1b
Guillaume <guillaume.richer@bell.net>
2022-11-22 10:44:49 -0500

    modify requete create-voiture

--- offre_voiture.php
+++ offre_voiture.php
@@ -26,7 +26,9 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
     if ($uploadOk == 0) {
         echo "<script> alert('Sorry, your file was not uploaded.') </script>";
-      } else {
+      } 
+      else
+       {
         if (move_uploaded_file($_FILES["imageInput"]["tmp_name"], $target_file)) {
             CreateVoiture($_POST['annee'], $_POST['couleur'], $_POST['marque'], $_POST['modele'], $_POST['type_voiture'], $_POST['odometre'], $_POST['type'], $_POST['porte'], $_POST['siege'], $_POST['traction'], $_POST['description'], $_POST['etat'], $_POST['prix'], $_POST['postal'], $_POST['dateDebut'], $_POST['dateFin'], $_FILES["imageInput"]["name"]);
 ;
